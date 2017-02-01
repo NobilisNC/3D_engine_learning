0b553fda6206b07fb99c9bc13257eef4858123f7
Tom HENOCH <tom.henoch@hotmail.fr>
2017-02-01 15:38:21 +0100

    Clean

--- src/Material.cpp
+++
@@ -1,8 +0,0 @@
-#include "Material.hpp"
-
-
-Material::Material(RGBColor _ambient, RGBColor _diffuse, RGBColor _specular, GLfloat _shininess, Texture *_text_diff, Texture *_text_spec)
-    : ambient(_ambient), diffuse(_diffuse), specular(_specular), shininess(_shininess), text_diff(_text_diff),  text_spec(_text_spec)
-{
-
-}
--- src/src/Scene.cpp
+++
@@ -1,6 +0,0 @@
-#include "include/Scene.hpp"
-
-Scene::Scene()
-{
-
-}
